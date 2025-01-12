8fa0c833fe5ad3f23427a8cb5d6146d3c4bf615c
krypto5863 <29824718+krypto5863@users.noreply.github.com>
2025-01-12 12:21:42 -0500

    Removed rogue using.

--- COM3D2.API.Core/COM3D2Api.cs
+++ COM3D2.API.Core/COM3D2Api.cs
@@ -5,7 +5,6 @@ using COM3D2API.Helpers;
 using COM3D2API.UI;
 using System.Security;
 using System.Security.Permissions;
-using COM3D2.CornerMessage;
 
 [module: UnverifiableCode]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
